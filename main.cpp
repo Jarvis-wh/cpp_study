04b71231ab02a1fed854a7e121c141499999b789
wanghan <wanghan@mova-tech.com>
2026-08-01 17:42:55 +0800

    done

--- main.cpp
+++ main.cpp
@@ -1,41 +1,16 @@
 #include <iostream>
 #include <iomanip>
 
-// class Shape { ... };
-class Shape {
-public:
-    virtual double area() const = 0; // pure virtual function
-    virtual ~Shape() {} // virtual destructor
-};
-// class Square : public Shape { ... };
-class Square : public Shape {
-private:
-    double side;
-public:
-    Square(double s) : side(s) {};
-    double area() const override {
-        return side * side;
-    }
-};
-// class Circle : public Shape { ... };
-class Circle : public Shape {
-private:
-    double r;
-public:
-    Circle(double radius) : r(radius) {};
-    double area() const override {
-        return 3.14159 * r * r;
-    }
-};
+template <typename T>
+T sumPair(T a, T b) {
+    return a + b;
+}
 
 int main() {
-    double s, r;
-    std::cin >> s >> r;
-    std::cout << std::fixed << std::setprecision(2);
-    Shape* a = new Square(s);
-    Shape* b = new Circle(r);
-    std::cout << a->area() << "\n";
-    std::cout << b->area() << "\n";
-    delete a; delete b;
+    int a, b;
+    double x, y;
+    std::cin >> a >> b >> x >> y;
+    std::cout << sumPair(a, b) << "\n";
+    std::cout << std::fixed << std::setprecision(2) << sumPair(x, y) << "\n";
     return 0;
 }
