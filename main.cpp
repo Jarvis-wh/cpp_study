9c8da2a64864f5f58d42438654d5d229a506033c
wanghan <wanghan@mova-tech.com>
2026-08-01 17:34:18 +0800

    done

--- main.cpp
+++ main.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
+#include <iomanip>
 
-class Rectangle {
-    // private members
-    int width;
-    int height;
+// class Shape { ... };
+class Shape {
 public:
-    // constructor + area() + perimeter()
-    Rectangle(int w, int h) : width{w}, height{h} {};
-    int area() const {return width * height;};
-    int perimeter() const {return 2 * (width + height);};
+    virtual double area() const = 0; // pure virtual function
+    virtual ~Shape() {} // virtual destructor
+};
+// class Square : public Shape { ... };
+class Square : public Shape {
+private:
+    double side;
+public:
+    Square(double s) : side(s) {};
+    double area() const override {
+        return side * side;
+    }
+};
+// class Circle : public Shape { ... };
+class Circle : public Shape {
+private:
+    double r;
+public:
+    Circle(double radius) : r(radius) {};
+    double area() const override {
+        return 3.14159 * r * r;
+    }
 };
 
 int main() {
-    int w, h;
-    std::cin >> w >> h;
-    Rectangle r{w, h};
-    std::cout << "area: " << r.area() << std::endl;
-    std::cout << "perimeter: " << r.perimeter() << std::endl;
+    double s, r;
+    std::cin >> s >> r;
+    std::cout << std::fixed << std::setprecision(2);
+    Shape* a = new Square(s);
+    Shape* b = new Circle(r);
+    std::cout << a->area() << "\n";
+    std::cout << b->area() << "\n";
+    delete a; delete b;
     return 0;
 }
