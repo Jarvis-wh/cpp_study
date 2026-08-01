a28388cc802f66dc5f9261542b78bce4af28217b
wanghan <wanghan@mova-tech.com>
2026-08-01 16:15:37 +0800

    done

--- main.cpp
+++ main.cpp
@@ -6,14 +6,10 @@ int main() {
     int n;
     std::cin >> n;
 
-    if(n%15 == 0) {
-        std::cout << "FizzBuzz" << std::endl;
-    } else if(n%3 == 0) {
-        std::cout << "Fizz" << std::endl;
-    } else if(n%5 == 0) {
-        std::cout << "Buzz" << std::endl;
-    } else {
-        std::cout << n << std::endl;
+    int sum{0};
+    for(int i = 1; i <= n; ++i) {
+        sum += i;
     }
+    std::cout << sum;
     return 0;
 }
